664b0b58b1964bff3588935ca1ff3c881fe20abe
alduris <152772325+alduris@users.noreply.github.com>
2025-01-25 04:49:10 -0500

    Works woo

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,6 +1,15 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using MonoMod.RuntimeDetour;
+using MonoMod.RuntimeDetour.HookGen;
+using RWCustom;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Security.Permissions;
+using System.Text;
 
 // Allows access to private members
 #pragma warning disable CS0618
@@ -24,10 +33,41 @@ sealed class Plugin : BaseUnityPlugin
     {
         orig(self, ID, fadeOutSeconds);
 
+        var filePath = Path.Combine(Custom.LegacyRootFolderDirectory(), "HookDebug.txt");
         try
         {
-            //
+            // Grab the thingies
+            Dictionary<MethodBase, Dictionary<Delegate, Stack<IDetour>>> hookMap = [];
+            Dictionary<MethodBase, object> hookEndpointMap = (Dictionary<MethodBase, object>)typeof(HookEndpointManager).GetField("HookEndpointMap", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+            foreach (var kvp in hookEndpointMap)
+            {
+                hookMap.Add(kvp.Key, ModManager.GetHookMap(kvp.Key));
+            }
+
+            // Order the thingies
+            var sortedList = hookMap.OrderBy(x => x.Key.DeclaringType.Namespace).ThenBy(x => x.Key.DeclaringType.Name).ThenBy(x => x.Key.Name).ToList();
+
+            // Generate the output string
+            var sb = new StringBuilder();
+            sb.AppendLine($"Generated at {DateTime.Now.ToUniversalTime():yyyy-MM-dd HH:mm:ss} UTC\n");
+            foreach (var kvp in sortedList)
+            {
+                sb.AppendLine($"{kvp.Key.DeclaringType.FullName}.{kvp.Key.Name}");
+                foreach (var hook in kvp.Value)
+                {
+                    sb.AppendLine($"    {hook.Key.Method.DeclaringType.FullName}.{hook.Key.Method.Name}  ({string.Join(", ", hook.Value.Select(x => x.GetType().Name))})");
+                }
+                sb.AppendLine();
+            }
+            File.WriteAllText(filePath, sb.ToString());
         }
-        catch { }
+        catch (Exception e)
+        {
+            try
+            {
+                File.WriteAllText(filePath, e.ToString());
+            }
+            catch { }
+        }
     }
 }
