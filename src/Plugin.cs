537db04e3092a72f75534eea6bb483a778297dbd
alduris <152772325+alduris@users.noreply.github.com>
2025-01-25 05:23:26 -0500

    Assembly info too

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Reflection;
 using System.Security.Permissions;
 using System.Text;
+using System.Text.RegularExpressions;
 
 // Allows access to private members
 #pragma warning disable CS0618
@@ -29,6 +30,9 @@ sealed class Plugin : BaseUnityPlugin
         On.ProcessManager.ActualProcessSwitch += ProcessManager_ActualProcessSwitch;
     }
 
+    // Idk why people would have it installed anywhere under the Users folder, but just to be safe...
+    private static string DoNotDoxxPeoplePlease(string path) => Regex.Replace(path, @"[/\\]Users[/\\][^/\\]+[/\\]", "\\Users\\USER\\");
+
     private void ProcessManager_ActualProcessSwitch(On.ProcessManager.orig_ActualProcessSwitch orig, ProcessManager self, ProcessManager.ProcessID ID, float fadeOutSeconds)
     {
         orig(self, ID, fadeOutSeconds);
@@ -36,7 +40,7 @@ sealed class Plugin : BaseUnityPlugin
         var filePath = Path.Combine(Custom.LegacyRootFolderDirectory(), "HookDebug.txt");
         try
         {
-            // Grab the thingies
+            // Grab all hooks
             Dictionary<MethodBase, Dictionary<Delegate, Stack<IDetour>>> hookMap = [];
             Dictionary<MethodBase, object> hookEndpointMap = (Dictionary<MethodBase, object>)typeof(HookEndpointManager).GetField("HookEndpointMap", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
             foreach (var kvp in hookEndpointMap)
@@ -44,25 +48,55 @@ sealed class Plugin : BaseUnityPlugin
                 hookMap.Add(kvp.Key, ModManager.GetHookMap(kvp.Key));
             }
 
+            // Grab all assemblies
+            bool successfulLoadedAssemblies = true;
+            Dictionary<string, string> loadedAssemblyStrings = [];
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var ass in assemblies)
+            {
+                try
+                {
+                    loadedAssemblyStrings.Add(ass.GetName().Name, $"{ass.FullName}, Location={DoNotDoxxPeoplePlease(ass.Location)}");
+                }
+                catch (Exception ex)
+                {
+                    successfulLoadedAssemblies = false;
+                    Logger.LogError(ex);
+                }
+            }
+
             // Order the thingies
-            var sortedList = hookMap.OrderBy(x => x.Key.DeclaringType.Namespace).ThenBy(x => x.Key.DeclaringType.Name).ThenBy(x => x.Key.Name).ToList();
+            var sortedList = hookMap.OrderBy(x => x.Key.DeclaringType.Namespace).ThenBy(x => x.Key.DeclaringType.Name).ThenBy(x => x.Key.Name).ToList(); // scary type (you do not want to know)
+            List<string> sortedAsses = loadedAssemblyStrings.OrderBy(x => x.Key).Select(x => x.Value).ToList(); // not scary type
 
             // Generate the output string
             var sb = new StringBuilder();
             sb.AppendLine($"Generated at {DateTime.Now.ToUniversalTime():yyyy-MM-dd HH:mm:ss} UTC\n");
+
             foreach (var kvp in sortedList)
             {
                 sb.AppendLine($"{kvp.Key.DeclaringType.FullName}.{kvp.Key.Name}");
                 foreach (var hook in kvp.Value)
                 {
-                    sb.AppendLine($"    {hook.Key.Method.DeclaringType.FullName}.{hook.Key.Method.Name}  ({string.Join(", ", hook.Value.Select(x => x.GetType().Name))})");
+                    sb.AppendLine($"    ({string.Join(", ", hook.Value.Select(x => x.GetType().Name))}) {hook.Key.Method.DeclaringType.FullName}.{hook.Key.Method.Name}");
                 }
                 sb.AppendLine();
             }
+
+            sb.AppendLine("\nLOADED ASSEMBLIES:");
+            if (!successfulLoadedAssemblies)
+                sb.AppendLine("NOTE: Errored loading some assemblies! See BepInEx/LogOutput.log for more details. Errored assemblies are skipped in this list.");
+            foreach (var ass in sortedAsses)
+            {
+                sb.Append("    ");
+                sb.AppendLine(ass);
+            }
+
             File.WriteAllText(filePath, sb.ToString());
         }
         catch (Exception e)
         {
+            Logger.LogError(e);
             try
             {
                 File.WriteAllText(filePath, e.ToString());
